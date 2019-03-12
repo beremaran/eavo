a4a6904715b126ccf8c0f7b4a6d2a2c22a48002f
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 15:01:33 +0300

    create Container type

---
+++ domain/entities/container.go
@@ -0,0 +1,11 @@
+package entities
+
+import "beremaran/eavo/domain/types"
+
+type Container struct {
+	Size types.Vector3i
+}
+
+func (c *Container) Volume() int {
+	return c.Size.X * c.Size.Y * c.Size.Z
+}
