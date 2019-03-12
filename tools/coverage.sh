55771fcd3233c9121e2769fc1d778fa5186d30f4
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 23:45:51 +0300

    prevent package coverage overrides

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -5,10 +5,12 @@ PKG_LIST=$(go list ./... | grep -v /vendor/)
 mkdir -p cover;
 
 for package in ${PKG_LIST}; do
-    go test -covermode=count -coverprofile "cover/${package##*/}.cov" "$package" ;
+    COV_PATH=$(echo "${package}" | tr / _)
+    COV_PATH="cover/${COV_PATH}.cov"
+    go test -covermode=count -coverprofile ${COV_PATH} "$package" ;
 done
 
-echo "mode: count\n" >> cover/coverage.cov
+echo "mode: count" >> cover/coverage.cov
 tail -q -n +2 cover/*.cov >> cover/coverage.cov
 go tool cover -func=cover/coverage.cov
 rm -rf cover;
