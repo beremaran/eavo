df4017f4f4b6e4b750ad6976e473df239f273982
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 17:04:08 +0300

    fix coverage script
     - remove coverage output from previous build
     - write a mode line to the combined coverage file

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -2,9 +2,13 @@
 
 PKG_LIST=$(go list ./... | grep -v /vendor/)
 
+rm -rf cover;
+mkdir -p cover;
+
 for package in ${PKG_LIST}; do
     go test -covermode=count -coverprofile "cover/${package##*/}.cov" "$package" ;
 done
 
+echo "mode: count\n" >> cover/coverage.cov
 tail -q -n +2 cover/*.cov >> cover/coverage.cov
 go tool cover -func=cover/coverage.cov
