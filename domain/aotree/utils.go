43d8600e3e4f1a6a69b4966fa011af2e08643aba
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 17:18:43 +0300

    document utils.go

--- domain/aotree/utils.go
+++ domain/aotree/utils.go
@@ -1,6 +1,6 @@
 package aotree
 
-//ContainsNode
+//ContainsNode searches for a node in given node slice
 func ContainsNode(arr []*AoNode, n *AoNode) bool {
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == n {
