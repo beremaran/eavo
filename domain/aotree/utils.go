1d93b8fe9c79f15a7c443d474fec07f6b854b6de
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 20:49:30 +0300

    create MapToBoxes utility function

--- domain/aotree/utils.go
+++ domain/aotree/utils.go
@@ -1,5 +1,7 @@
 package aotree
 
+import "gitlab.com/beremaran/eavo/domain/entities"
+
 //ContainsNode searches for a node in given node slice
 func ContainsNode(arr []*AoNode, n *AoNode) bool {
 	for i := 0; i < len(arr); i++ {
@@ -9,4 +11,16 @@ func ContainsNode(arr []*AoNode, n *AoNode) bool {
 	}
 
 	return false
+}
+
+//MapToBoxes maps a tree's store nodes to their bounding boxes
+func MapToBoxes(node *AoNode) ([]*entities.Box, error) {
+	var boxes []*entities.Box
+	nodes := node.Root().StoreNodes()
+
+	for i := 0; i < len(nodes); i++ {
+		boxes = append(boxes, &nodes[i].Box)
+	}
+
+	return boxes, nil
 }
