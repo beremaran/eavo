4a7bd1085531be0e4ff2b895a3fe8c9bcae6e36e
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-12 22:19:56 +0300

    implement tournament selection

---
+++ application/genetic/selector_tournament.go
@@ -0,0 +1,36 @@
+package genetic
+
+import (
+	"gitlab.com/beremaran/eavo/domain/genetic"
+	"math/rand"
+)
+
+type TournamentSelector struct {
+	TournamentSize int
+}
+
+func (s *TournamentSelector) Select(population []*genetic.Individual) (*genetic.Individual, error) {
+	return s.tournament(population, 1)
+}
+
+func (s *TournamentSelector) tournament(population []*genetic.Individual, level int) (*genetic.Individual, error) {
+	if level == s.TournamentSize {
+		return population[rand.Intn(len(population))], nil
+	}
+
+	left, err := s.tournament(population, level+1)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := s.tournament(population, level+1)
+	if err != nil {
+		return nil, err
+	}
+
+	if left.FitnessScore > right.FitnessScore {
+		return left, nil
+	}
+
+	return right, nil
+}
