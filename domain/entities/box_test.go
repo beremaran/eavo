00e3649f3e05895d5e3884f01342a3aea347cb2a
Berke Emrecan Arslan <beremaran@gmail.com>
2019-03-13 15:01:27 +0300

    create unit tests for Box.Fits && Box.Rotate

--- domain/entities/box_test.go
+++ domain/entities/box_test.go
@@ -45,3 +45,55 @@ func TestBox_Clone(t *testing.T) {
 		t.Errorf("incorrect cloned box size")
 	}
 }
+
+func TestBox_Fits(t *testing.T) {
+	box1 := Box{
+		Size: types.Vector3i{
+			X: 10, Y: 10, Z: 10,
+		},
+	}
+
+	box2 := Box{
+		Size: types.Vector3i{
+			X: 5, Y: 5, Z: 5,
+		},
+	}
+
+	box3 := Box{
+		Size: types.Vector3i{
+			X: 10, Y: 20, Z: 10,
+		},
+	}
+
+	if !box2.Fits(&box1) {
+		t.Errorf("(5, 5, 5) actually fits (10, 10, 10) box, you know?")
+	}
+
+	if box3.Fits(&box1) {
+		t.Errorf("(10, 20, 10) box can't be fitting into (10, 10, 10)")
+	}
+}
+
+func TestBox_Rotate(t *testing.T) {
+	box := Box{
+		Size: types.Vector3i{
+			X: 1, Y: 2, Z: 3,
+		},
+	}
+
+	rX := box.Rotate(types.AxisX)
+	rY := box.Rotate(types.AxisY)
+	rZ := box.Rotate(types.AxisZ)
+
+	if rX.Size.Y != 3 || rX.Size.Z != 2 {
+		t.Errorf("x rotation didn't work")
+	}
+
+	if rY.Size.X != 3 || rY.Size.Z != 1 {
+		t.Errorf("y rotation didn't work")
+	}
+
+	if rZ.Size.X != 2 || rZ.Size.Y != 1 {
+		t.Errorf("z rotation didn't work")
+	}
+}
